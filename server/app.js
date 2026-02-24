1c08065c1cf170650876c1327f2b3afbb03d8abc
NAVEEN0701K <naveenkaver0701@gmail.com>
2026-02-24 13:02:22 +0530

    fix(deploy): Configure server and client for Vercel deployment

--- server/app.js
+++ server/app.js
@@ -50,7 +50,4 @@ if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'developme
   });
 }
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,2 +1,7 @@
 ﻿require('dotenv').config();
-require('./app');
+const app = require('./app');
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
