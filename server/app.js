a45a16ae06d5aaeb01ba91e196a3d03e17e5af10
NAVEEN0701K <naveenkaver0701@gmail.com>
2026-02-24 18:54:56 +0530

    fix(client): Fix static axios base URL to support Vercel proxy, remove static fallback in express

--- server/app.js
+++ server/app.js
@@ -39,15 +39,5 @@ const path = require('path');
 
 app.use(errorHandler);
 
-// Serve frontend in production
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'development') {
-  // Set static folder
-  app.use(express.static(path.join(__dirname, '../client/dist')));
-
-  // Any route that is not an API route will be redirected to index.html
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
-  });
-}
 
 module.exports = app;
